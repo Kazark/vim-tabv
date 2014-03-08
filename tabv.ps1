73c1f761c43ac6c85859c2b827cadca656c4d741
Kazark <kazark@zoho.com>
2014-03-08 11:33:39 -0500

    Added Tabjsv and Tabcxxv functions to scripts
    for Bash and Powershell

--- tabv.ps1
+++ tabv.ps1
@@ -7,6 +7,13 @@ function vs($file1, $file2) {
 }
 
 function Tabv($name) {
-    vs inc/$1.hpp src/$1.cpp
+    vs inc/$name.hpp src/$name.cpp
 }
 
+function Tabcxxv($name) {
+    vs inc/$name.hpp src/$name.cpp
+}
+
+function Tabjsv($name) {
+    vs unittests/$name.src src/$name.spec.js
+}
--- tabv.sh
+++ tabv.sh
@@ -4,3 +4,11 @@ function Tabv {
     vs inc/$1.hpp src/$1.cpp
     #vim --cmd "e src/$1.cpp | vs inc/$1.hpp | sp unittest/$1Tests.cpp"
 }
+function Tabcxxv {
+    vs inc/$1.hpp src/$1.cpp
+    #vim --cmd "e src/$1.cpp | vs inc/$1.hpp | sp unittest/$1Tests.cpp"
+}
+function Tabjsv {
+    vs unittests/$1.spec.js src/$1.js
+    #vim --cmd "e src/$1.cpp | vs inc/$1.hpp | sp unittest/$1Tests.cpp"
+}
